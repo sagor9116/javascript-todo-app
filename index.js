69d9e44394a9a2f2d81d71c180e91cda62a214db
sagor9116 <sagor.roy9116@gmail.com>
2022-12-29 17:04:20 +0600

    done checking completed todo and udated an existing todo

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const todoListsEl = document.querySelector(".todo-list-container")
 
 // todos
 let todos = []
+let editTodoId = -1
 
 //form submit
 todoForm.addEventListener("submit", (e) => {
@@ -31,13 +32,21 @@ const saveTodo = () => {
   } else if (duplicatedTodo) {
     alert("todo already exists")
   } else {
-    const newTodo = {
-      todoText: todoInputValue,
-      isCompleted: false,
+    if (editTodoId >= 0) {
+      todos = todos.map((todo, index) => ({
+        ...todo,
+        todoText: index === editTodoId ? todoInputValue : todo.todoText,
+      }))
+      editTodoId = -1
+    } else {
+      const newTodo = {
+        todoText: todoInputValue,
+        isCompleted: false,
+        status: "active",
+      }
+      // pushing newTodos to todos array
+      todos.push(newTodo)
     }
-    // pushing newTodos to todos array
-    todos.push(newTodo)
-    console.log(todos)
   }
 
   // clearing the input field after submission
@@ -50,36 +59,67 @@ const renderTodos = () => {
   todoListsEl.innerHTML = ""
 
   //render todos
-  todos.map((todo, index) => {
+  todos.forEach((todo, index) => {
     todoListsEl.innerHTML += `
-  <div class="todo-lists" id=${index}>
-    <div class="check-mark checked">
-      <img src="/images/icon-check.svg" alt="" />
-    </div>
-    <!-- check-mark -->
+    <div class="todo-lists" id=${index}>
+      <div class='check-mark ${
+        todo.isCompleted ? "checked" : ""
+      }' data-action ="check" >
+      ${todo.isCompleted ? '<img src="/images/icon-check.svg" alt="" />' : ""}
+      </div>
+      <!-- check-mark -->
   
-    <div class="todo-text">
-      <span>${todo.todoText}</span>
-    </div>
-    <!-- todo-text -->
+      <div class='todo-text ${
+        todo.isCompleted ? "checked" : ""
+      }' id=${index} data-action ="check">
+          ${todo.todoText}
+      </div>
+      <!-- todo-text -->
 
-    <div class="todo-options">
-      <span class="edit">
-        <i class="fa-sharp fa-solid fa-pen-to-square"></i>
-      </span>
-      <span class="delete">
-        <i class="fa-sharp fa-solid fa-trash"></i>
-      </span>
+      <div class="todo-options">
+        <span class="edit" id=${index}>
+          <i class="fa-sharp fa-solid fa-pen-to-square" data-action = "edit"></i>
+        </span>
+        <span class="delete" id=${index}>
+          <i class="fa-sharp fa-solid fa-trash" data-action = "delete"></i>
+        </span>
+      </div>
+      <!-- .todo-options -->
     </div>
-    <!-- .todo-options -->
-  </div>
+  <!-- .todo-lists -->
   `
   })
 }
 
-/* 
+// adding event listener
 
+todoListsEl.addEventListener("click", (e) => {
+  const target = e.target
+  console.log(target)
+  const parentEl = target.parentNode
+  console.log(parentEl)
+  // todo id
+  const todoId = Number(parentEl.id)
+  console.log(todoId)
 
-  
-  
-*/
+  // target action
+  const action = target.dataset.action
+  action === "check" && checkTodo(todoId)
+  action === "edit" && editTodo(todoId)
+})
+
+// check a todo
+const checkTodo = (todoId) => {
+  todos = todos.map((todo, index) => ({
+    ...todo,
+    isCompleted: index === todoId ? !todo.isCompleted : todo.isCompleted,
+    status: "completed",
+  }))
+  renderTodos()
+}
+
+// edit a todo
+const editTodo = (todoId) => {
+  todoInput.value = todos[todoId].todoText
+  editTodoId = todoId
+}
